dde43aafa38d0e32b571eadf2abe359899d3b791
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-10-25 23:46:28 +0600

    woc code

--- src/Components/WhyOurCompany.jsx
+++ src/Components/WhyOurCompany.jsx
@@ -1,4 +1,6 @@
-
+import { FaRegStar } from "react-icons/fa";
+import { LuShield } from "react-icons/lu";
+import { GoZap } from "react-icons/go";
 
 const WhyOurCompany = () => {
     return (
@@ -17,13 +19,90 @@ const WhyOurCompany = () => {
                 </div>
                 <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 md:gap-12 items-center ">
                <div className="relative w-full flex justify-center order-2 lg:order-1">
-                <div className="w-full max-w-md lg:max-w-lg xl:max-w-xl h-[460px] md:h-[500] lg:h-[520px] overflow-hidden bg-gray-50 shadow-lg md:shadow-xl absolute top-0 z-0 rounded-[30%_10%_20%_70%/60%_30%_70%_30%]">
-
+                <div className="w-full max-w-md lg:max-w-lg xl:max-w-xl h-[560px] md:h-[500] lg:h-[520px] overflow-hidden bg-gray-50 shadow-lg md:shadow-xl absolute top-0 z-0 rounded-[30%_10%_20%_70%/60%_30%_70%_30%]"
+                data-aos = 'fade-right'
+                data-aos-delay = '100'
+                >
                 </div>
-                    </div>
+                <div className=" relative z-10 p-4 sm:p-6 w-full max-w-md">
 
+                <div className="flex items-start gap-4 sm:gap-6 p-4 transition-all bg-transparent " data-aos = 'fade-right'
+                data-aos-delay = '200' >
+                <div className="flex-shrink-0 size-10 sm:size-12 flex items-center justify-center">
+                <FaRegStar className="size-5 sm:size-6 md:size-7 text-blue-600"/>    
                 </div>
+                <div>
+                <h3 className="text-lg sm:text-xl font-semibold text-gray-900 mb-2">
+                    Excellence Guaranteed
+                </h3>
+                <p className="text-gray-600 text-sm sm:text-base">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dignissimos aut, autem quia deserunt cumque eius 
+                </p>
+                </div>    
+                </div>
+
+                 <div className="flex items-start gap-4 sm:gap-6 p-4 transition-all bg-transparent " data-aos = 'fade-right'
+                data-aos-delay = '200' >
+                <div className="flex-shrink-0 size-10 sm:size-12 flex items-center justify-center">
+                <LuShield className="size-5 sm:size-6 md:size-7 text-purple-600"/>    
+                </div>
+                <div>
+                <h3 className="text-lg sm:text-xl font-semibold text-gray-900 mb-2">
+                    Trust & Reliability
+                </h3>
+                <p className="text-gray-600 text-sm sm:text-base">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dignissimos aut, autem quia deserunt cumque eius 
+                </p>
+                </div>    
+                </div>
+
+                <div className="flex items-start gap-4 sm:gap-6 p-4 transition-all bg-transparent " data-aos = 'fade-right'
+                data-aos-delay = '200' >
+                <div className="flex-shrink-0 size-10 sm:size-12 flex items-center justify-center">
+                <GoZap className="size-5 sm:size-6 md:size-7 text-pink-600"/>    
+                </div>
+                <div>
+                <h3 className="text-lg sm:text-xl font-semibold text-gray-900 mb-2">
+                    Innovation Driven
+                </h3>
+                <p className="text-gray-600 text-sm sm:text-base">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dignissimos aut, autem quia deserunt cumque eius  
+                </p>
+                </div>    
+                </div>
+             </div>
             </div>
+        <div className="relative order-1 lg:order-2 ">
+         <div className="bg-gradient-to-br from-yellow-600 to-pink-600 rounded-2xl md:rounded-3xl p-6 md:p-8 flex flex-col justify-center"
+         data-aos = 'zoom-in'
+         data-aos-delay= '200'>
+        <div className="max-w-md mx-auto text-center">
+         <div className="size-16 sm:size-20 rounded-full bg-white/20 flex items-center justify-center mx-auto mb-4 sm:mb-6">
+         <FaRegStar className="size-8 sm:size-10 "/>
+         </div>
+         <h3 className="text-xl sm:text-2xl font-bold mb-3 sm:mb-4">
+            98% Customer Satisfaction
+         </h3>
+         <p className="m-4 sm:text-base">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore maxime sequi dolorum fugit.
+         </p>
+
+         <div className="flex justify-center gap-3 sm:gap-4">
+            <div className="text-center"
+            data-aos = 'fade-up'
+            data-aos-delay='400'>
+            <div className="text-2xl sm:text-3xl font-bold">500+</div>
+            <div className="text-xs sm:text-sm">Project Completed</div>    
+            </div> 
+         </div>
+         
+            </div>
+            </div>   
+
+            </div>
+
+        </div>
+        </div>
 
         </section>
     );

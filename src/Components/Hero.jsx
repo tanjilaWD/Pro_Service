30d4acb494694bd32d368759cdfff18fb4a5b340
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-10-29 10:56:08 +0600

    complate

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -11,54 +11,51 @@ const Hero = () => {
               Professionels{" "}
               <span className="font-bold text-black block lg:inline">
                 Service <span className="text-green-500">.</span>
-              </span>
-            </h2>
-           <div
-           data-aos='fade-right'
-           data-aos-delay= '100'
-            className="flex gap-3 mt-4 justify-center lg:justify-start">
-          <span className="w-5 h-5 rounded-full border-2 border-pink-500"></span>
-          <span className="w-5 h-5 rounded-full border-2 border-yellow-500"></span>
-          <span className="w-5 h-5 rounded-full border-2 border-green-500"></span>
+               </span>
+             </h2>
+            <div
+              data-aos='fade-right'
+              data-aos-delay= '100'
+              className="flex gap-3 mt-4 justify-center lg:justify-start">
+                <span className="w-5 h-5 rounded-full border-2 border-pink-500"></span>
+                <span className="w-5 h-5 rounded-full border-2 border-yellow-500"></span>
+                <span className="w-5 h-5 rounded-full border-2 border-green-500"></span>
+             </div>
+              <div className="flex flex-col lg:grid lg:grid-cols-2 gap-6 lg:gap-12 items-center lg:items-start mt-6">
+           {/* card 1 */}
+              <div
+                 data-aos='zoom-in'
+                data-aos-delay= '100'
+
+              className="bg-white w-72 rounded-3xl p-6 border border-gray-100 shadow-xl transition">
+              <div className="ml-4 size-10 flex items-center justify-center rounded-lg bg-pink-100 mb-4">
+              <RiPokerHeartsLine className="text-pink-600 size-6" />
+              </div>
+              <h3 className="ml-4 text-lg font-semibold text-gray-800">
+                Service Subtitle One
+              </h3>
+              <p className="ml-4 text-gray-500 text-sm mt-2">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing.
+              </p>
+             </div>
+
+            {/* card 2 */}
+            <div
+             data-aos='zoom-in'
+             data-aos-delay= '300'
+             className="bg-white w-72 rounded-3xl p-6 border border-gray-100 shadow-xl transition">
+             <div className="ml-4 size-10 flex items-center justify-center rounded-lg bg-yellow-100 mb-4">
+             <FiAward className="text-yellow-600 size-6" />
+             </div>
+              <h3 className="ml-4 text-lg font-semibold text-gray-800">
+                    Service Subtitle Two
+              </h3>
+              <p className="ml-4 text-gray-500 text-sm mt-2">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing.
+              </p>
+              </div>
+            </div>
            </div>
-  <div className="flex flex-col lg:grid lg:grid-cols-2 gap-6 lg:gap-12 items-center lg:items-start mt-6">
-  {/* card 1 */}
-  <div
-  data-aos='zoom-in'
-   data-aos-delay= '100'
-
-  className="bg-white w-72 rounded-3xl p-6 border border-gray-100 shadow-xl transition">
-    <div className="ml-4 size-10 flex items-center justify-center rounded-lg bg-pink-100 mb-4">
-      <RiPokerHeartsLine className="text-pink-600 size-6" />
-    </div>
-    <h3 className="ml-4 text-lg font-semibold text-gray-800">
-      Service Subtitle One
-    </h3>
-    <p className="ml-4 text-gray-500 text-sm mt-2">
-      Lorem ipsum dolor sit amet, consectetur adipisicing.
-    </p>
-  </div>
-
-  {/* card 2 */}
-  <div
-  data-aos='zoom-in'
-   data-aos-delay= '300'
-  className="bg-white w-72 rounded-3xl p-6 border border-gray-100 shadow-xl transition">
-    <div className="ml-4 size-10 flex items-center justify-center rounded-lg bg-yellow-100 mb-4">
-      <FiAward className="text-yellow-600 size-6" />
-    </div>
-    <h3 className="ml-4 text-lg font-semibold text-gray-800">
-      Service Subtitle Two
-    </h3>
-    <p className="ml-4 text-gray-500 text-sm mt-2">
-      Lorem ipsum dolor sit amet, consectetur adipisicing.
-    </p>
-  </div>
-</div>
-
-      
-
-       </div>
         <div
         data-aos='fade-left'
        data-aos-delay= '400'
